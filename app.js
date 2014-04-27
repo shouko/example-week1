f0369f257bcb32cc378865371674418021c15ac1
Howard Yu <howard@Mac-Mini.local>
2014-04-27 14:18:53 +0800

    Added slider

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-/**
- * @overview
- *
- * @author
- * @version 2014/04/26
- */
+console.log("hello world!");
 
 var http = require("http");
 var port = 1337;
-var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
 
-http.createServer(function (req, res) {
-  res.writeHeader(200, {"Content-Type": "text/html"});
-  
-  var data = "<html><head></head><body>"
-  request.get(url, function (err, body, response) {
+http.createServer(function (req, res){
 
-    response = JSON.parse(response);
-    response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
-    });
-    
-    data += "</body></html>";
-    res.end(data);
-  });
+	var query = require('url').parse(req.url,true).query;
+	console.log("Request from: " + req.connection.remoteAddress + "\n" + query.fn + "\n");
 
-}).listen(port);
+	if(query.fn){
 
-console.log("start server port: " + port);
+		fs = require('fs');
+		// provide requested css, js, images, while preventing directory traversal
+		fs.readFile('theme/'+ query.fn.replace(/\.\./g,""), 'utf8', function (err,data) {
+		  if (err) {
+	  		res.writeHeader(404, {"Content-Type": "text/html"});
+			res.end("404 - Not Found");
+		    return console.log(err);
+		  }
+		  res.end(data);
+		});
 
+	}else{
+	
+	res.writeHeader(200, {"Content-Type": "text/html"});
 
+	var data = '<html><head>';
+	data += '<link href="?fn=js-image-slider.css" rel="stylesheet" type="text/css" />';
+    data += '<script src="?fn=js-image-slider.js" type="text/javascript"></script>';
+	data += '</head><body><div id="sliderFrame"><div id="slider">';
+
+	var request = require("request");
+	var url = "http://graph.facebook.com/2014NTUIMCAMP/photos?type=uploaded";
+
+	request.get(url, function (err, body, response) {
+
+		response = JSON.parse(response);
+	    response.data.forEach(function (val, idx) {
+
+	      data += "<img src='" + val.images[0].source + "' />";
+		});
+
+			data += "</div></body></html>";
+			res.end(data);
+	});
+
+	}
+}
+).listen(port);
+
+console.log("server created! " + port);
